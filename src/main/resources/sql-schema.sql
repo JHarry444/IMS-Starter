5107bd13aebac72c72df35a1a6d2629cb0350e24
jharry444 <jordan.harrison@qa.com>
2020-09-15 10:32:49 +0100

    Fixed SQL

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -3,7 +3,7 @@ CREATE SCHEMA IF NOT EXISTS `ims`;
 USE `ims` ;
 CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `first_name` VARCHAR(40) NULL DEFAULT NULL,
-    `surname` VARCHAR(40) NULL DEFAULT NULL,
+    `first_name` VARCHAR(40) DEFAULT NULL,
+    `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS `customers`;
 CREATE TABLE IF NOT EXISTS `customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `first_name` VARCHAR(40) NULL DEFAULT NULL,
-    `surname` VARCHAR(40) NULL DEFAULT NULL,
+    `first_name` VARCHAR(40) DEFAULT NULL,
+    `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
