6fa8739c7b8d58e5eedd948170239fd9935b6652
Dan Bennett <dbennett@qa.com>
2021-01-29 13:02:48 +0000

    Added UML + UML.png + SQL-Schema.sql

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,8 +1,37 @@
 DROP TABLE IF EXISTS `customers`;
 
 CREATE TABLE IF NOT EXISTS `customers` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `customer_id` INT(11) NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) DEFAULT NULL,
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `items`;
+
+CREATE TABLE IF NOT EXISTS `items` (
+	`item_id` INT (11) NOT NULL AUTO_INCREMENT,
+	`item_name` VARCHAR (40) DEFAULT NULL,
+	`item_price` INT (5) DEFAULT NULL,
+	PRIMARY KEY (`item_id`)
+);
+
+DROP TABLE IF EXISTS `orders`;
+
+CREATE TABLE IF NOT EXISTS `orders` (
+	`order_id` INT (11) NOT NULL AUTO_INCREMENT,
+	`customer_id` INT (11) NOT NULL,
+	PRIMARY KEY (`order_id`),
+	FOREIGN KEY (`customer_id`) REFERENCES customers(`customer_id`)
+);
+
+DROP TABLE IF EXISTS `order_item`;
+
+CREATE TABLE IF NOT EXISTS `order_item`(
+	`order_item_id` INT (11) NOT NULL AUTO_INCREMENT,
+	`order_id` INT (11) NOT NULL,
+	`item_id` INT (11) NOT NULL,
+	PRIMARY KEY (`order_item_id`),
+	FOREIGN KEY (`order_id`) REFERENCES orders(`order_id`),
+	FOREIGN KEY (`item_id`) REFERENCES items(`item_id`)
 );
