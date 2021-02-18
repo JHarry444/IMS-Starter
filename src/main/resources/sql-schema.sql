3b11cfe2cac62bd26d1a70ee4ea4ff6b0fa12a2e
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-02-18 09:39:21 +0000

    Added sql queries for items and orders

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -10,3 +10,20 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(40) DEFAULT NULL,
+	`price` FLOAT(20) DEFAULT NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+	`orderid` INT(11) NOT NULL AUTO_INCREMENT,
+	`customerid` INT(11) NOT NULL,
+	`itemid` INT(11) NOT NULL,
+	`quantity` INT(15) NOT NULL,
+	PRIMARY KEY(orderid),
+	FOREIGN KEY(customerid) REFERENCES customers(id),
+	FOREIGN KEY(itemid) REFERENCES items(id)
+); 
