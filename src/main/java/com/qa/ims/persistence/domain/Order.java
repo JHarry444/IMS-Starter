29a04cf151caba730ea8e903956a84c4414fc370
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-02-19 10:07:20 +0000

    IMS-31 #in-progress Created order object class

---
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -0,0 +1,86 @@
+package com.qa.ims.persistence.domain;
+
+public class Customer {
+
+	private Long id;
+	private String firstName;
+	private String surname;
+
+	public Customer(String firstName, String surname) {
+		this.setFirstName(firstName);
+		this.setSurname(surname);
+	}
+
+	public Customer(Long id, String firstName, String surname) {
+		this.setId(id);
+		this.setFirstName(firstName);
+		this.setSurname(surname);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	@Override
+	public String toString() {
+		return "id:" + id + " first name:" + firstName + " surname:" + surname;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((surname == null) ? 0 : surname.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (getFirstName() == null) {
+			if (other.getFirstName() != null)
+				return false;
+		} else if (!getFirstName().equals(other.getFirstName()))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (surname == null) {
+			if (other.surname != null)
+				return false;
+		} else if (!surname.equals(other.surname))
+			return false;
+		return true;
+	}
+
+}
