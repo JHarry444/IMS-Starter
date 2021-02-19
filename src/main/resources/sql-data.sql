0242625902caf40e6065225add7ebe0f5d20cd99
kieran ryder <kieranlryder@gmail.com>
2021-02-19 11:02:24 +0000

    QAP-44 #comment Added dummy data to sql-data to test items

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,2 +1,2 @@
 INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
-INSERT INTO `ims`.`item` (`name`, `price`) VALUES ('cat', 3.50);
+INSERT INTO `ims`.`item` (`name`, `price`) VALUES ('test_item', 22.22);
--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,8 +1,18 @@
 DROP TABLE IF EXISTS `customers`;
 
-CREATE TABLE IF NOT EXISTS `customers` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `customers`
+(
+    `id`         INT(11) NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) DEFAULT NULL,
-    `surname` VARCHAR(40) DEFAULT NULL,
+    `surname`    VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
+);
+DROP TABLE IF EXISTS `item`;
+
+CREATE TABLE IF NOT EXISTS `item`
+(
+    `iditem` INT         NOT NULL AUTO_INCREMENT,
+    `name`   VARCHAR(45) NOT NULL,
+    `price`  DOUBLE      NOT NULL,
+    PRIMARY KEY (`iditem`)
 );
