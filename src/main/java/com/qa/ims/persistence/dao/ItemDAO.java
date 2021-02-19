81e864c848aebb0ddc72133349715d710b3d7786
kieran ryder <kieranlryder@gmail.com>
2021-02-19 11:43:05 +0000

    QAP-46 #comment Implemented Create function in itemDAO

--- src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
@@ -1,12 +1,19 @@
 package com.qa.ims.persistence.dao;
 
 import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.DBUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-public class ItemDAO implements Dao<Item>{
+public class ItemDAO implements Dao<Item> {
+    public static final Logger LOGGER = LogManager.getLogger();
+
     @Override
     public List<Item> readAll() {
         return null;
@@ -19,6 +26,17 @@ public class ItemDAO implements Dao<Item>{
 
     @Override
     public Item create(Item item) {
+        try (Connection connection = DBUtils.getInstance().getConnection();
+             PreparedStatement statement = connection
+                     .prepareStatement("INSERT INTO item(name, price) VALUES (?, ?)")) {
+            statement.setString(1, item.getName());
+            statement.setDouble(2, item.getPrice());
+            statement.executeUpdate();
+            return readLatest();
+        } catch (Exception e) {
+            LOGGER.debug(e);
+            LOGGER.error(e.getMessage());
+        }
         return null;
     }
 
