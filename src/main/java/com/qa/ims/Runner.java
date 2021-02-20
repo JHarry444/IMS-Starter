f5e21ea30f8a5b17967c30357b68180605ff37f4
Nik-Pap <nikospap8@hotmail.com>
2021-02-20 11:04:47 +0000

    a few changes

--- src/main/java/com/qa/ims/Runner.java
+++ src/main/java/com/qa/ims/Runner.java
@@ -1,7 +1,6 @@
 package com.qa.ims;
 
 import java.sql.SQLException;
-import java.util.Scanner;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,7 +8,7 @@ import org.apache.logging.log4j.Logger;
 public class Runner {
 
 	public static final Logger LOGGER = LogManager.getLogger();
-	private static Scanner scanner = new Scanner(System.in);
+	
 
 	public static void main(String[] args) throws SQLException {
 		IMS ims = new IMS();
