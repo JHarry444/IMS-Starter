9e8946ae11fa121a18fd96a2d44bb312a9ed28dd
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-02-22 15:04:52 +0000

    IMS-58 Updated queries for not null columns

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -6,14 +6,14 @@ USE `ims` ;
 
 CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `first_name` VARCHAR(40) DEFAULT NULL,
-    `surname` VARCHAR(40) DEFAULT NULL,
+    `first_name` VARCHAR(40) DEFAULT NOT NULL,
+    `surname` VARCHAR(40) DEFAULT NOT NULL,
     PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `ims`.`items` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(40) DEFAULT NULL,
+	`name` VARCHAR(40) DEFAULT NOT NULL,
 	`price` FLOAT(20) DEFAULT NOT NULL,
 	PRIMARY KEY (`id`)
 ); 
