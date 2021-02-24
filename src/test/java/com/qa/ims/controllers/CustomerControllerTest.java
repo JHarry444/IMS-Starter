37583dc9facd1b294b06c5ae7fa5048b0c40c715
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-02-24 14:25:19 +0000

    IMS-33 Mockito JUnit 5 compatibility

--- src/test/java/com/qa/ims/controllers/CustomerControllerTest.java
+++ src/test/java/com/qa/ims/controllers/CustomerControllerTest.java
@@ -10,14 +10,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.qa.ims.controller.CustomerController;
 import com.qa.ims.persistence.dao.CustomerDAO;
 import com.qa.ims.persistence.domain.Customer;
 import com.qa.ims.utils.Utils;
 
-@ExtendWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class CustomerControllerTest {
 
 	@Mock
