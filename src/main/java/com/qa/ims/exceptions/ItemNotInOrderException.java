6e3d554e81db6b0e31eadcbd80ae108dd479c8b2
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-02-24 09:54:54 +0000

    IMS-66 Created new exception classes

---
+++ src/main/java/com/qa/ims/exceptions/ItemNotInOrderException.java
@@ -0,0 +1,16 @@
+package com.qa.ims.exceptions;
+
+public class ItemNotInOrderException extends RuntimeException {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 3195262845082636649L;
+
+    /**
+     *
+     */
+
+   
+    
+}
