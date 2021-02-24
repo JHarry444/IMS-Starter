4014331967a037d0a72073473e99f223d78f4e66
Ifan Clarke <Ifan>
2021-02-24 14:08:35 +0000

    SQL orderline fixed

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -10,3 +10,24 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `items` (
+`itemid` INT(11) NOT NULL AUTO_INCREMENT,
+`product` VARCHAR(40) DEFAULT NULL,
+`price` DOUBLE DEFAULT NULL,
+PRIMARY KEY(`itemid`)
+);
+
+CREATE TABLE IF NOT EXISTS `orders` (
+`orderid` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11),
+`total` DOUBLE DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `orderline`(
+`orderlineid` int(11) NOT NULL AUTO_INCREMENT,
+`orderid` int(11),
+`itemid` int(11),
+`itemquant` int(11),
+PRIMARY KEY(`orderlineid`)
+);
