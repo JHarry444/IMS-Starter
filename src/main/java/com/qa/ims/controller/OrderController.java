c7619945c2c89f62611f901cb64a4676a782ef26
Ifan Clarke <Ifan>
2021-02-25 13:08:41 +0000

    fixing ordercontroller

--- src/main/java/com/qa/ims/controller/OrderController.java
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -6,24 +6,29 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.qa.ims.persistence.dao.OrderDAO;
+import com.qa.ims.persistence.dao.OrderlineDAO;
 import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.persistence.domain.Orderline;
 import com.qa.ims.utils.Utils;
 
-public class OrderController implements CrudController<Order>  {
+import java.util.Map;
+import java.util.HashMap;
+
+public class OrderController implements CrudController<Order> {
 
 	public static final Logger LOGGER = LogManager.getLogger();
-	
+
 	private OrderDAO orderDAO;
+	private OrderlineDAO orderlineDAO;
 	private Utils utils;
-	
-	public OrderController(OrderDAO orderDAO, Utils utils) {
+
+	public OrderController(OrderDAO orderDAO, OrderlineDAO orderlineDAO, Utils utils) {
 		super();
 		this.orderDAO = orderDAO;
+		this.orderlineDAO = orderlineDAO;
 		this.utils = utils;
 	}
-	
-	
-	
+
 	@Override
 	public List<Order> readAll() {
 		List<Order> orders = orderDAO.readAll();
@@ -33,29 +38,74 @@ public class OrderController implements CrudController<Order>  {
 		return orders;
 	}
 
-	
 	@Override
 	public Order create() {
 		LOGGER.info("Please enter the Customer ID for the order");
 		Long id = utils.getLong();
-		LOGGER.info("Please enter the Order Line ID for the order");
-		Long orderlineid = utils.getLong();
-		Order order = orderDAO.create(new Order(id, orderlineid));
-		LOGGER.info("Order Created");
-		return order;
+
+		Map<Long, Long> itemmap = new HashMap();
+
+		LOGGER.info("Would you like to add an item? (Yes or no)");
+		String answer = utils.getString();
+
+		while (answer.toLowerCase().equals("yes")) {
+
+			LOGGER.info("Please enter the Item ID for the order");
+			Long itemid = utils.getLong();
+			LOGGER.info("Please enter the quantity of the item");
+			Long itemquant = utils.getLong();
+			itemmap.put(itemid, itemquant);
+			LOGGER.info("Would you like to add another item?(Yes or no)");
+			answer = utils.getString();
 		}
+
+		Order order = orderDAO.create(new Order(id));
+		Long orderid = order.getOrderid();
+		System.out.println(orderid);
+		for (Long itemid : itemmap.keySet()) {
+			Long itemquant = itemmap.get(itemid);
+			orderlineDAO.create(new Orderline(orderid, itemid, itemquant));
+		}
+
+		LOGGER.info("Order Created");
+
+		return order;
+
+	}
 
 	@Override
 	public Order update() {
-		LOGGER.info("Please enter the order id of the order you would like to update");
+		LOGGER.info("Please enter the ID for the order you wish to update");
 		Long orderid = utils.getLong();
-		LOGGER.info("Please enter an item id");
-		Long id = utils.getLong();
-		LOGGER.info("Please enter an Order Line ID");
-		Long orderlineid  = utils.getLong();
-		Order order = orderDAO.update(new Order(orderid, id, orderlineid));
+
+		Map<Long, Long> itemmap = new HashMap();
+
+		LOGGER.info("Would you like to add an item? (Yes or no)");
+		String answer = utils.getString();
+
+		while (answer.toLowerCase().equals("yes")) {
+
+			LOGGER.info("Please enter the Item ID for the order");
+			Long itemid = utils.getLong();
+			LOGGER.info("Please enter the quantity of the item");
+			Long itemquant = utils.getLong();
+			itemmap.put(itemid, itemquant);
+			LOGGER.info("Would you like to add another item?(Yes or no)");
+			answer = utils.getString();
+		}
+
+		Order order = orderDAO.create(new Order(orderid));
+		Long orderlineid = order.getOrderlineid();
+	
+		for (Long itemid : itemmap.keySet()) {
+			Long itemquant = itemmap.get(itemid);
+			orderlineDAO.create(new Orderline(orderid, itemid, itemquant));
+		}
+
 		LOGGER.info("Order Updated");
+
 		return order;
+
 	}
 
 	@Override
