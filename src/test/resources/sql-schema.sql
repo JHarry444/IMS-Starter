ac223fae0f31725129a5126399984f32c841f239
Ifan Clarke <Ifan>
2021-02-25 19:17:30 +0000

    test sql schema up to date

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -5,25 +5,29 @@ CREATE TABLE IF NOT EXISTS `customers` (
     `first_name` VARCHAR(40) DEFAULT NULL,
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
-);
+)ENGINE = INNODB;
 
 CREATE TABLE IF NOT EXISTS `items` (
 `itemid` INT(11) NOT NULL AUTO_INCREMENT,
 `product` VARCHAR(40) DEFAULT NULL,
 `price` DOUBLE DEFAULT NULL,
 PRIMARY KEY(`itemid`)
-);
+)ENGINE = INNODB;
 
 CREATE TABLE IF NOT EXISTS `orders` (
 `orderid` int(11) NOT NULL AUTO_INCREMENT,
 `id` int(11),
-PRIMARY KEY (`orderid`)
-);
+PRIMARY KEY (`orderid`),
+FOREIGN KEY (`id`) REFERENCES `customers`(`id`)
+)ENGINE = INNODB;
 
 CREATE TABLE IF NOT EXISTS `orderline`(
 `orderlineid` int(11) NOT NULL AUTO_INCREMENT,
 `orderid` int(11),
 `itemid` int(11),
 `itemquant` int(11),
-PRIMARY KEY(`orderlineid`)
-);
+PRIMARY KEY(`orderlineid`),
+FOREIGN KEY (`orderid`) REFERENCES `orders`(`orderid`)
+ON DELETE CASCADE,
+FOREIGN KEY (`itemid`) REFERENCES `items` (`itemid`)
+)ENGINE = INNODB;
