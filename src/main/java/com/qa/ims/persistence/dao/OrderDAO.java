63a8377e029c7cdd01c060bdf669fe048ace2d3f
Ifan Clarke <Ifan>
2021-02-25 13:07:37 +0000

    pushing orderdao changes

--- src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
@@ -6,31 +6,44 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.controller.OrderController;
 import com.qa.ims.utils.DBUtils;
 
 public class OrderDAO implements Dao<Order> {
 public static final Logger LOGGER = LogManager.getLogger();
 
+
+public String convertMapToString(Map<Long, Long> itemmap) {
+	StringBuilder mapToString = new StringBuilder();
+	for (Long key:itemmap.keySet()) {
+		mapToString.append(key + "=" + itemmap.get(key) + ", ");
+	}
+	mapToString.delete(mapToString.length()-2, mapToString.length()).append("}");
+	return mapToString.toString();	
+}
+
 	
 	@Override
 	public Order modelFromResultSet(ResultSet resultSet) throws SQLException {
-		Long orderid = resultSet.getLong("orderid");
 		Long id = resultSet.getLong("id");
-		Long orderlineid = resultSet.getLong("orderlineid");
-		return new Order (orderid, id, orderlineid);
+		Long orderid = resultSet.getLong("orderid");
+		return new Order (id, orderid);
 	}
+	
 	
 	@Override
 	public List<Order> readAll() {
 		try (Connection connection = DBUtils.getInstance().getConnection();
 				Statement statement = connection.createStatement();
-				ResultSet resultSet = statement.executeQuery("SELECT * FROM orders");) {
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM orders INNER JOIN orderline ON orders.orderid = orderline.orderid;");) {
 			List<Order> orders = new ArrayList<>();
 			while (resultSet.next()) {
 				orders.add(modelFromResultSet(resultSet));
@@ -59,56 +72,46 @@ public static final Logger LOGGER = LogManager.getLogger();
 	@Override
 	public Order create(Order orders) {
 		try (Connection connection = DBUtils.getInstance().getConnection();
-				PreparedStatement statement = connection
-						.prepareStatement("INSERT INTO orders(id, orderlineid) VALUES (?, ?)");) {
-			statement.setLong(1, orders.getId());
-			statement.setLong(2, orders.getOrderLineId());
-			statement.executeUpdate();
-			return readLatest();
-		} catch (Exception e) {
-			LOGGER.debug(e);
-			LOGGER.error(e.getMessage());
-		}
-		return null;
+				PreparedStatement statement = connection.prepareStatement("INSERT INTO orders(id) VALUES (?);");) {
+
+				statement.setLong(1, orders.getId());
+				statement.executeUpdate();
+				return readLatest();
+			}
+			catch (Exception e) {
+				LOGGER.debug(e);
+				LOGGER.error(e.getMessage());
+			}
+
+			return null;
 	}
 	
 	@Override
 	public Order read(Long orderid) {
-		try (Connection connection = DBUtils.getInstance().getConnection();
-				PreparedStatement statement = connection.prepareStatement("SELECT * FROM orders WHERE orderid = ?");) {
-			statement.setLong(1, orderid);
-			try (ResultSet resultSet = statement.executeQuery();) {
-				resultSet.next();
-				return modelFromResultSet(resultSet);
-			}
-		} catch (Exception e) {
-			LOGGER.debug(e);
-			LOGGER.error(e.getMessage());
-		}
 		return null;
 	}
 
 	@Override
 	public Order update(Order orders) {
 		try (Connection connection = DBUtils.getInstance().getConnection();
-				PreparedStatement statement = connection
-						.prepareStatement("UPDATE orders SET id = ?, orderlineid = ? WHERE orderid = ?");) {
-			statement.setLong(1, orders.getId());
-			statement.setLong(2, orders.getOrderLineId());
-			statement.setLong(3, orders.getOrderId());
-			statement.executeUpdate();
-			return read(orders.getOrderId());	
-		} catch (Exception e) {
-			LOGGER.debug(e);
-			LOGGER.error(e.getMessage());
-		}
-		return null;
+				PreparedStatement statement = connection.prepareStatement("INSERT INTO orders(id) VALUES (?);");) {
+
+				statement.setLong(1, orders.getId());
+				statement.executeUpdate();
+				return readLatest();
+			}
+			catch (Exception e) {
+				LOGGER.debug(e);
+				LOGGER.error(e.getMessage());
+			}
+
+			return null;
 	}
 
 	@Override
 	public int delete(long orderid) {
 		try (Connection connection = DBUtils.getInstance().getConnection();
-				PreparedStatement statement = connection.prepareStatement("DELETE FROM orders WHERE orderid = ?");) {
+				PreparedStatement statement = connection.prepareStatement("DELETE FROM orders WHERE orderid  = ?; DELETE FROM orderline WHERE orderid = ?;");) {
 			statement.setLong(1, orderid);
 			return statement.executeUpdate();
 		} catch (Exception e) {
