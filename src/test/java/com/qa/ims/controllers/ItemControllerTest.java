957a4dd5e13b2f9ab98518616515d37b4c69f770
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-02-25 13:13:08 +0000

    IMS-37 Testing Controller classes

--- src/test/java/com/qa/ims/controllers/ItemControllerTest.java
+++ src/test/java/com/qa/ims/controllers/ItemControllerTest.java
@@ -1,5 +1,93 @@
 package com.qa.ims.controllers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.qa.ims.controller.ItemController;
+import com.qa.ims.persistence.dao.ItemDAO;
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.Utils;
+
+@ExtendWith(MockitoExtension.class)
 public class ItemControllerTest {
-    
+
+	@Mock
+	private Utils utils;
+
+	@Mock
+	private ItemDAO dao;
+
+	@InjectMocks
+	private ItemController controller;
+
+	@Test
+	public void testCreate() {
+		final String NAME = "apple";
+        final Double PRICE = 1.5D;
+		final Item created = new Item(NAME, PRICE);
+
+		Mockito.when(utils.getString()).thenReturn(NAME);
+        Mockito.when(utils.getDouble()).thenReturn(PRICE);
+		Mockito.when(dao.create(created)).thenReturn(created);
+
+		assertEquals(created, controller.create());
+
+		Mockito.verify(utils, Mockito.times(1)).getString();
+        Mockito.verify(utils, Mockito.times(1)).getDouble();
+		Mockito.verify(dao, Mockito.times(1)).create(created);
+	}
+
+	@Test
+	public void testReadAll() {
+		List<Item> Items = new ArrayList<>();
+		Items.add(new Item(1L, "apple", 1.5D));
+
+		Mockito.when(dao.readAll()).thenReturn(Items);
+
+		assertEquals(Items, controller.readAll());
+
+		Mockito.verify(dao, Mockito.times(1)).readAll();
+	}
+
+	@Test
+	public void testUpdate() {
+		Item updated = new Item(1L, "banana", 2.5D);
+
+		Mockito.when(this.utils.getLong()).thenReturn(1L);
+		Mockito.when(this.utils.getString()).thenReturn(updated.getName());
+        Mockito.when(this.utils.getDouble()).thenReturn(updated.getPrice());
+		Mockito.when(this.dao.update(updated)).thenReturn(updated);
+
+		assertEquals(updated, this.controller.update());
+
+		Mockito.verify(this.utils, Mockito.times(1)).getLong();
+		Mockito.verify(this.utils, Mockito.times(1)).getString();
+        Mockito.verify(this.utils, Mockito.times(1)).getDouble();
+		Mockito.verify(this.dao, Mockito.times(1)).update(updated);
+	}
+
+	@Test
+	public void testDelete() {
+		final long ID = 1L;
+
+		Mockito.when(utils.getLong()).thenReturn(ID);
+		Mockito.when(utils.getString()).thenReturn("Y");
+		Mockito.when(dao.delete(ID)).thenReturn(1);
+
+		assertEquals(1, this.controller.delete());
+
+		Mockito.verify(utils, Mockito.times(1)).getLong();
+        Mockito.verify(utils, Mockito.times(1)).getString();
+		Mockito.verify(dao, Mockito.times(1)).delete(ID);
+	}
+
 }
