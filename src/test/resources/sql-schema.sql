41a5f5dad3d165bfcbce8a9cc787cd8ec9a4206a
Ifan Clarke <Ifan>
2021-02-26 08:46:32 +0000

    sorted out schema for test

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,25 +1,29 @@
+DROP TABLE IF EXISTS `orderline`;
+DROP TABLE IF EXISTS `orders`;
 DROP TABLE IF EXISTS `customers`;
+DROP TABLE IF EXISTS `items`;
 
 CREATE TABLE IF NOT EXISTS `customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) DEFAULT NULL,
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
-)ENGINE = INNODB;
+);
+
 
 CREATE TABLE IF NOT EXISTS `items` (
 `itemid` INT(11) NOT NULL AUTO_INCREMENT,
 `product` VARCHAR(40) DEFAULT NULL,
 `price` DOUBLE DEFAULT NULL,
 PRIMARY KEY(`itemid`)
-)ENGINE = INNODB;
+);
 
 CREATE TABLE IF NOT EXISTS `orders` (
 `orderid` int(11) NOT NULL AUTO_INCREMENT,
 `id` int(11),
 PRIMARY KEY (`orderid`),
 FOREIGN KEY (`id`) REFERENCES `customers`(`id`)
-)ENGINE = INNODB;
+);
 
 CREATE TABLE IF NOT EXISTS `orderline`(
 `orderlineid` int(11) NOT NULL AUTO_INCREMENT,
@@ -30,4 +34,4 @@ PRIMARY KEY(`orderlineid`),
 FOREIGN KEY (`orderid`) REFERENCES `orders`(`orderid`)
 ON DELETE CASCADE,
 FOREIGN KEY (`itemid`) REFERENCES `items` (`itemid`)
-)ENGINE = INNODB;
+);
