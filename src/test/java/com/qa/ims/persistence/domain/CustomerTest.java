c5863e25ae60e062e70a4228e9bddfa9c3a2ca59
Sam Picco Tarullo <68030022+starullo@users.noreply.github.com>
2021-05-31 16:57:01 -0500

    added unit tests to CustomerTest class with 81.1% coverage

--- src/test/java/com/qa/ims/persistence/domain/CustomerTest.java
+++ src/test/java/com/qa/ims/persistence/domain/CustomerTest.java
@@ -1,16 +1,70 @@
 package com.qa.ims.persistence.domain;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import nl.jqno.equalsverifier.EqualsVerifier;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CustomerTest {
 
+//	CONSTRUCTORS
 	@Test
-	public void testEquals() {
-		EqualsVerifier.simple().forClass(Customer.class).verify();
+	public void constructorTest1() {
+		Customer c = new Customer((long) 1, "Bob", "Smith");
+		assertEquals(1, c.getId());
+		assertEquals("Bob", c.getFirstName());
+		assertEquals("Smith", c.getSurname());
 	}
 	
-
+	@Test
+	public void constructorTest2() {
+		Customer c = new Customer("Al", "Johnson");
+		assertNull(c.getId());
+		assertEquals("Al", c.getFirstName());
+		assertEquals("Johnson", c.getSurname());
+	}
+	
+//	SETTERS
+	@Test
+	public void settersTest() {
+		Customer c = new Customer("Jack", "Robinson");
+		assertNull(c.getId());
+		c.setId((long) 23);
+		assertEquals(23, c.getId());
+		assertEquals("Jack", c.getFirstName());
+		c.setFirstName("Jackie");
+		assertEquals("Jackie", c.getFirstName());
+		assertEquals("Robinson", c.getSurname());
+		c.setSurname("Biggs");
+		assertEquals("Biggs", c.getSurname());
+	}
+	
+//	OVERRIDES
+	@Test
+	public void toStringTest() {
+		Customer c = new Customer("Will", "Johnson");
+		String s = "id:null first name:Will surname:Johnson";
+		assertEquals(s, c.toString());
+	}
+	
+	@Test void hashCodeTest() {
+		Customer c = new Customer("Jessie", "Jones");
+		Customer c2 = new Customer("Bobby", "James");
+		assertFalse(c.hashCode() == c2.hashCode());
+	}
+	
+	@Test void equalsTest() {
+		Customer c1 = new Customer((long) 3, "James", "Jackson");
+		Customer c2 = new Customer((long) 4, "Susie", "Jones");
+		assertFalse(c1.equals(c2));
+		
+		Customer c3 = new Customer("Pete", "Ratburn");
+		Customer c4 = new Customer("Chuck", "Robinson");
+		assertFalse(c3.equals(c4));
+		c3.setFirstName("Chuck");
+		assertFalse(c3.equals(c4));
+		c3.setSurname("Robinson");
+		assertTrue(c3.equals(c4));
+		
+	}
 
 }
