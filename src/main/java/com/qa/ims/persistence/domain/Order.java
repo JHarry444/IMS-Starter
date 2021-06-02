6487b672c4f34d9bfa1f6236bc5797a0ec555dea
Sam Picco Tarullo <68030022+starullo@users.noreply.github.com>
2021-06-02 17:13:51 -0500

    ST: Modified order class

--- src/main/java/com/qa/ims/persistence/domain/Order.java
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -1,43 +1,34 @@
 package com.qa.ims.persistence.domain;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 
 public class Order {
 
 	private Long id;
 	private Long customerId;
-	private BigDecimal total;
 	private LocalDate orderedOn;
 	
 //	CONSTRUCTORS
-	public Order(Long customerId, double total, LocalDate orderedOn) {
+	public Order(Long customerId, LocalDate orderedOn) {
 		this.setCustomerId(customerId);
-		this.setTotal(total + "");
 		this.setOrderedOn(orderedOn);
 	}
 	
-	public Order(Long customerId, String total, LocalDate orderedOn) {
+	public Order(Long customerId) {
 		this.setCustomerId(customerId);
-		this.setTotal(total);
-		this.setOrderedOn(orderedOn);
+		this.setOrderedOn(LocalDate.now());
 	}
 	
-	public Order(Long id, Long customerId, double total, LocalDate orderedOn) {
+	public Order(Long id, Long customerId) {
 		this.setId(id);
 		this.setCustomerId(customerId);
-		this.setTotal(total + "");
-		this.setOrderedOn(orderedOn);
+		this.setOrderedOn(LocalDate.now());
 	}
 	
-	public Order(Long id, Long customerId, String total, LocalDate orderedOn) {
+	public Order(Long id, Long customerId, LocalDate orderedOn) {
 		this.setId(id);
 		this.setCustomerId(customerId);
-		this.setTotal(total);
 		this.setOrderedOn(orderedOn);
 	}
 	
@@ -50,18 +41,13 @@ public class Order {
 		return this.customerId;
 	}
 	
-	public String getTotalString() {
-		NumberFormat nf = NumberFormat.getCurrencyInstance(Locale.US);
-		return nf.format(this.total.doubleValue());
-	}
-	
-	public double getTotalDouble() {
-		return this.total.doubleValue();
-	}
-	
-	public String getOrderedOn() {
+	public String getOrderedOnString() {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		return orderedOn.format(formatter);
+	}
+	
+	public LocalDate getOrderedOnDate() {
+		return this.orderedOn;
 	}
 	
 //	SETTERS
@@ -73,26 +59,22 @@ public class Order {
 		this.customerId = newCustomerId;
 	}
 	
-	public void setTotal(String newTotal) {
-		BigDecimal nt = new BigDecimal(newTotal);
-		nt = nt.setScale(2, RoundingMode.CEILING);
-		this.total = nt;
-	}
-	
-	public void setTotal(double newTotal) {
-		BigDecimal nt = new BigDecimal(newTotal + "");
-		nt = nt.setScale(2, RoundingMode.CEILING);
-		this.total = nt;
-	}
-	
 	public void setOrderedOn(LocalDate newOrderedOn) {
 		this.orderedOn = newOrderedOn;
+	}
+	
+	public void setOrderedOn(String newOrderedOn) {
+		String[] dateArr = newOrderedOn.split("-", 0);
+		int year = Integer.parseInt(dateArr[0]);
+		int month = Integer.parseInt(dateArr[1]);
+		int day = Integer.parseInt(dateArr[2]);
+		this.orderedOn = LocalDate.of(year, month, day);
 	}
 	
 //	OVERRIDES
 	@Override
 	public String toString() {
-		return "id: " + this.getId() + ", customerId: " + this.getCustomerId() + ", total: " + this.getTotalDouble() + ", orderedOn: " + this.getOrderedOn();
+		return "id: " + this.getId() + ", customerId: " + this.getCustomerId() + ", orderedOn: " + this.getOrderedOnString();
 	}
 	
 	@Override
@@ -109,11 +91,6 @@ public class Order {
 			if (other.getCustomerId() != null)
 				return false;
 		} else if (!getCustomerId().equals(other.getCustomerId()))
-			return false;
-		if (total == null) {
-			if (other.total != null)
-				return false;
-		} else if (!getTotalString().equals(other.getTotalString()))
 			return false;
 		if (orderedOn == null) {
 			if (other.orderedOn != null)
