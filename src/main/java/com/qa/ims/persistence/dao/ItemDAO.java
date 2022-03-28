5c3ba263b61aa9d3de68b673fc2935fbc6f95dc4
Lee <leenock2012@yahoo.co.uk>
2022-03-28 16:15:47 +0100

    Added classes
    Begun working on the Item domain

---
+++ src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
@@ -0,0 +1,13 @@
+package com.qa.ims.persistence.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemDAO implements Dao<Item> {
+
+}
---
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -0,0 +1,8 @@
+package com.qa.ims.persistence.domain;
+
+public class Item {
+
+	private String name;
+	private String description;
+	private float price;
+}
