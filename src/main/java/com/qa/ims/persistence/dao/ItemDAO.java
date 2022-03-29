c5bcc60d2067b241338059b98102be7eb8ee9fe2
Lee <leenock2012@yahoo.co.uk>
2022-03-29 10:31:35 +0100

    BSE-6 Added ItemDAO
    Item DAO now contains all the correct methods. Future changes will be updates or fixes.

--- src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
@@ -8,44 +8,128 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Customer;
 import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.DBUtils;
 
 public class ItemDAO implements Dao<Item> {
 
-	@Override
-	public List<Item> readAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public static final Logger LOGGER = LogManager.getLogger();
 
-	@Override
-	public Item read(Long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Item create(Item t) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Item update(Item t) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int delete(long id) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
+	// This will essentially unwrap data from the sql table results row by row.
 	@Override
 	public Item modelFromResultSet(ResultSet resultSet) throws SQLException {
 		// TODO Auto-generated method stub
+		Long id = resultSet.getLong("id");
+		String name = resultSet.getString("name");
+		String description = resultSet.getString("description");
+		float price = resultSet.getFloat("price");
+		return new Item(id, name, description, price);
+	}
+
+	// This is going to read all of the items in the data base and return a list of
+	// them.
+	@Override
+	public List<Item> readAll() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM items");) {
+			List<Item> items = new ArrayList<>();
+			while (resultSet.next()) {
+				items.add(modelFromResultSet(resultSet));
+			}
+			return items;
+		} catch (SQLException e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return new ArrayList<>();
+	}
+
+	// This block will read a single item from the database depending on the given
+	// id.
+	@Override
+	public Item read(Long id) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection.prepareStatement("SELECT * FROM items WHERE id = ?");) {
+			statement.setLong(1, id);
+			try (ResultSet resultSet = statement.executeQuery();) {
+				resultSet.next();
+				return modelFromResultSet(resultSet);
+			}
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
 		return null;
+	}
+
+	// This block will read the most recently added item in the database.
+	public Item readLatest() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM items ORDER BY id DESC LIMIT 1");) {
+			resultSet.next();
+			return modelFromResultSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	// This will do exactly what it says, we will create an item with a name and
+	// description.
+	@Override
+	public Item create(Item item) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection
+						.prepareStatement("INSERT INTO items(name, description) VALUES(?, ?)");) {
+			statement.setString(1, item.getName());
+			statement.setString(2, item.getDescription());
+			statement.executeUpdate();
+			return readLatest();
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	// This will allows us to make changes to an item using their id to select the
+	// correct row.
+	@Override
+	public Item update(Item item) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection
+						.prepareStatement("UPDATE items SET name = ?, description = ? WHERE id = ?");) {
+			statement.setString(1, item.getName());
+			statement.setString(2, item.getDescription());
+			statement.setLong(3, item.getId());
+			statement.executeUpdate();
+			return read(item.getId());
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	// This will simply allow us to delete a given row when we use the matching id.
+	@Override
+	public int delete(long id) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection.prepareStatement("DELETE FROM items WHERE id = ?");) {
+			statement.setLong(1, id);
+			return statement.executeUpdate();
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return 0;
 	}
 
 }
