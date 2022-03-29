8c51fa3cdab7d39c7723dae901125f27813d5fbc
Lee <leenock2012@yahoo.co.uk>
2022-03-29 14:06:21 +0100

    BSE-6 Item Controller additions
    Added some of the necessary imports and created the Item List's readALL().

--- src/main/java/com/qa/ims/controller/ItemController.java
+++ src/main/java/com/qa/ims/controller/ItemController.java
@@ -2,14 +2,29 @@ package com.qa.ims.controller;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.ItemDAO;
 import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.Utils;
+
+// Similar to the customer controller, this will take in details on the items for CRUD functionality.
 
 public class ItemController implements CrudController<Item> {
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+	
+	private ItemDAO itemDAO;
+	private Utils utils;
 
 	@Override
 	public List<Item> readAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Item> items = itemDAO.readAll();
+		for (Item item : items) {
+			LOGGER.info(item);
+		}
+		return items;
 	}
 
 	@Override
---
+++ src/test/java/com/qa/ims/controllers/CustomerControllerIntegrationTest.java
@@ -0,0 +1,6 @@
+package com.qa.ims.controllers;
+
+public class CustomerControllerIntegrationTest {
+
+	CustomerController controller2 = new CustomerController(new CustomerDAO(), new Utils());
+}
