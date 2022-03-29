69be5ea7044a79b460329d859012a6f66434016d
Lee <leenock2012@yahoo.co.uk>
2022-03-29 10:56:08 +0100

    Added the ItemController
    Implemented necessary methods, will be working on the bodies.

--- src/main/java/com/qa/ims/controller/ItemController.java
+++ src/main/java/com/qa/ims/controller/ItemController.java
@@ -1,5 +1,33 @@
 package com.qa.ims.controller;
 
-public class ItemController {
+import java.util.List;
+
+import com.qa.ims.persistence.domain.Item;
+
+public class ItemController implements CrudController<Item> {
+
+	@Override
+	public List<Item> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item update() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
