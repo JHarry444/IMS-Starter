dffb41ef083e7dc9fb851f6ece44a090899501c3
Lee <leenock2012@yahoo.co.uk>
2022-03-29 09:08:38 +0100

    Updated Item.java
    Added, getters and setters alongside hashCode() and equals()

--- src/main/java/com/qa/ims/persistence/domain/Item.java
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -1,9 +1,91 @@
 package com.qa.ims.persistence.domain;
 
+import java.util.Objects;
+
 public class Item {
 
+	private Long id;
 	private String name;
 	private String description;
 	private float price;
+	
+	public Item(String name, String description) {
+		this.setName(name);
+		this.setDescription(description);
+	}
+	
+	public Item(Long id, String name, String description, float price) {
+		this.setId(id);
+		this.setName(name);
+		this.setDescription(description);
+		this.setPrice(price);
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+
+	public float getPrice() {
+		return price;
+	}
+
+
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, id, name, price);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		return Objects.equals(description, other.description) && Objects.equals(id, other.id)
+				&& Objects.equals(name, other.name) && Float.floatToIntBits(price) == Float.floatToIntBits(other.price);
+	}
+	
 	
 }
