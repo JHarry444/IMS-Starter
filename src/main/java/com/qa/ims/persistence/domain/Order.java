30c32dfd0d0a672190fbf1e31f4ea516af8a7832
Lee <leenock2012@yahoo.co.uk>
2022-03-30 15:45:21 +0100

    BSE-23 Created an order domain
    Not 100% with this so it may need changing later down the line. DO NOT CLOSE BRANCH

--- src/main/java/com/qa/ims/persistence/domain/Order.java
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -1,8 +1,58 @@
 package com.qa.ims.persistence.domain;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Order {
 
 	private Long id;
+	private Customer customer;
+	private ArrayList<Item> item;
 	
+	public Order() {
+	}
 	
+	public Order(Long id, Customer customer, ArrayList<Item> item) {
+		this.id = id;
+		this.customer = customer;
+		this.item = item;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public ArrayList<Item> getItem() {
+		return item;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(customer, id, item);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(customer, other.customer) && Objects.equals(id, other.id)
+				&& Objects.equals(item, other.item);
+	}
+
 }
